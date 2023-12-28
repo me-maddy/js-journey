e2af3098cc8540994a657f37ea2f5a2f81fe7156
Innocent-Maddy <103171939+me-maddy@users.noreply.github.com>
2023-12-28 11:32:16 +0530

    start object

---
+++ basics/object.js
@@ -0,0 +1,17 @@
+// const obj = {};
+// const obj1 = Object("Object Hai Re");
+// const obj2 = Object(true);
+const obj3 = Object({
+  name: "Madan",
+  age: 24,
+  fullname: { first: "Madan", last: "Sharma" },
+});
+const obj4 = new Object({ name: "Raghuraj" });
+
+// console.log(obj1);
+// console.log(obj2);
+// console.log(obj1 instanceof Object);
+// console.log(typeof obj1);
+// console.log(obj);
+console.log(obj3);
+console.log(obj4);
