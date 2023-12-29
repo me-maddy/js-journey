846125978d68ad717f32c16109979336b129ab17
Innocent-Maddy <103171939+me-maddy@users.noreply.github.com>
2023-12-29 13:15:43 +0530

    function && scope

---
+++ basics/function.js
@@ -0,0 +1,54 @@
+// function addOne(anyNum) {
+//   const userName = "mdandev30";
+//   console.log(anyNum + 1);
+//   console.log(userName);
+//   console.log(this.userName);
+//   //   console.log(this);
+// }
+
+// function addOne(anyNum) {
+//   return anyNum + 1;
+// }
+
+// console.log(this);
+// addOne(8);
+
+// const result = addOne(8);
+// console.log(result);
+
+// addTwo(); // function declaration can be called before initialisation
+
+// function addTwo() {
+//   console.log("Addtwo function is called");
+// }
+
+// addOne(); // Can't call function expression before initialisation.
+
+// const addOne = function () {
+//   console.log("Add one is invoked");
+// };
+
+// addOne();
+// const addOne = () => {
+//   console.log("Add one is invoked");
+// };
+
+// addOne();// it will work
+
+// addTwo(); //
+
+// *******************Arrow function*************************
+// const sayMyName = () => {
+//   console.log(this);
+//   console.log("My Name is Madan");
+//   return this;
+// };
+
+// const sayName = function () {
+//   console.log(this);
+// };
+
+// sayMyName();
+// const obj = sayMyName();
+// console.log(obj);
+// sayName();
---
+++ basics/scope.js
@@ -0,0 +1,68 @@
+//  var, let and const
+
+// var a = 20;
+// let b = 30;
+// const c = 10;
+
+// var a;
+// var b;
+// var c;
+
+// console.log("Outside: ", a, b, c);
+
+// console.log(a);
+
+// var a = 100;
+// var a;
+// let a;
+// let b = 200;
+// let c = 300;
+
+// console.log("Outside: ", a, b, c);
+
+// {
+//   var a = 200;
+//   a = 200;
+//   let b = 300;
+//   let a = 200;
+//   let b = 300;
+//   const c = 100;
+//   console.log("Inside Block: ", a, b, c);
+// }
+// console.log("Outside: ", a, b, c);
+
+// const username = "mdandev";
+// console.log(username);
+
+// if (true) {
+//   const username = "dev30";
+//   //   console.log(username);
+//   if (true) {
+//     const username = "madandev30";
+//     const website = "dev30.com";
+//     console.log("Username : " + username + " : " + website);
+//   }
+//   console.log("Username : " + username);
+// }
+// console.log("Username : " + username);
+
+// const user = "Madan";
+// const fullName = "Madan Sharma";
+
+// function userName() {
+//   const user = "Ritesh";
+//   function inner() {
+//     const lastName = "Sharma";
+//     const user = "Manjeet";
+//     // console.log(user);
+//     console.log(fullName);
+//   }
+//   console.log(lastName);
+
+//   inner();
+//   console.log(user);
+//   console.log(fullName);
+// }
+// console.log(user);
+
+// userName();
