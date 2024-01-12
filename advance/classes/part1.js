7a6540d212acb6c611be59b7f9e37d703ae8004d
Innocent-Maddy <103171939+me-maddy@users.noreply.github.com>
2024-01-12 08:54:51 +0530

    updation in part1 of classes

--- advance/classes/part1.js
+++ advance/classes/part1.js
@@ -26,3 +26,22 @@
 /** ****************************************************************************************
  * **************** Above same thing can be achieved by creating function ******************
  * ******************************************************************************************/
+
+function CreateUser(username, age) {
+  this.username = username;
+  this.age = age;
+}
+
+CreateUser.prototype.getInfo = function () {
+  console.log(`${this.username} is ${this.age} years old.`);
+};
+
+// CreateUser.prototype.getInfo(); // output :- undefined is undefined years old.
+
+const user1 = new CreateUser("Madan", 23);
+console.log(user1); // output :- CreateUser { username: 'Madan', age: 23 }
+user1.getInfo(); // output :- Madan is 23 years old.
+
+const user2 = new CreateUser("Manjeet", 21);
+console.log(user2); // output :- CreateUser { username: 'Manjeet', age: 21 }
+user2.getInfo(); // output :- Manjeet is 21 years old.
