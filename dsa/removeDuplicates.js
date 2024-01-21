4a12cdea3b2cf0ea9fc15b0f23f0ae1b55e6c8dd
Innocent-Maddy <103171939+me-maddy@users.noreply.github.com>
2024-01-21 23:49:34 +0530

    more in dsa

--- dsa/removeDuplicates.js
+++ dsa/removeDuplicates.js
@@ -14,6 +14,7 @@
 // };
 
 // *********** Second Approach ************
+
 // const removeDuplicates = function (arr) {
 //   for (let i = 0; i < arr.length - 1; i++) {
 //     if (arr[i] === arr[i + 1]) {
