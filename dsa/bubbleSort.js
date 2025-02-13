2f604dbc2dca63b6398792f7feedc1c2e591d04b
Madan <madan.sharma281572@gmail.com>
2025-02-13 10:39:40 +0530

    solved a problem to sort an array using bubble sort

---
+++ dsa/bubbleSort.js
@@ -0,0 +1,28 @@
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+// }
+
+// Another Approach :-
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i >= 1; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+const arr = [4, 2, 5, 8, 3, 1];
+bubbleSort(arr);
+console.log(arr);
